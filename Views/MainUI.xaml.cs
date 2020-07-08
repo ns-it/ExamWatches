9a3ce2d2a65aca73d275321e818da7410955c5dc
Nawzat <nawzat.sibai@gmail.com>
2020-07-08 12:32:18 +0300

    Initialization commit

---
+++ Views/MainUI.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ExamWatches.Views
+{
+    /// <summary>
+    /// Interaction logic for MainUI.xaml
+    /// </summary>
+    public partial class MainUI : Window
+    {
+        public MainUI()
+        {
+            InitializeComponent();
+        }
+    }
+}
