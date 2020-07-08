ef9682ca47eb9c2f9405bcfd9245671abf783f5b
Nawzat <nawzat.sibai@gmail.com>
2020-07-08 12:32:18 +0300

    Initialization commit

---
+++ Views/Schedualin_2.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ExamWatches.Views
+{
+    /// <summary>
+    /// Interaction logic for Schedualin_2.xaml
+    /// </summary>
+    public partial class Schedualin_2 : Window
+    {
+        public Schedualin_2()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Views/WatcherSchedualing.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ExamWatches.Views
+{
+    /// <summary>
+    /// Interaction logic for WatcherSchedualing.xaml
+    /// </summary>
+    public partial class WatcherSchedualing : Window
+    {
+        public WatcherSchedualing()
+        {
+            InitializeComponent();
+        }
+    }
+}
